cc8db286809a7eec1e6509a37e435ab4b188abb9
GerBawn <lingchen410@163.com>
2015-05-27 21:27:14 +0800

    幽灵按钮
    使用CSS3中的transition, scale, rotate属性为按钮添加动画

--- buttons/main.js
+++ buttons/main.js
@@ -1,3 +1,20 @@
 /**
  * Created by ling on 2015/5/26.
  */
+
+$(function(){
+    $('.link .button').hover(function(){
+        var title = $(this).attr('data');
+        var parentLeft = $(this).position().left + 10;
+        $('.tip em').text(title);
+        var tip = $('.tip');
+        var left = parentLeft - (tip.outerWidth() - $(this).outerWidth()) / 2;
+        tip.css({'left' : left + 'px'}).animate({'top': 140, 'opacity': 1}, 300);
+    }, function(){
+        var tip = $('.tip');
+        if(!tip.is(':animated')){
+            tip.animate({'top': 100, 'opacity': 0}, 300);
+        }
+    });
+
+});
